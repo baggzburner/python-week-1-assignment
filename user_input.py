cfd0daba8b4768e46dbcb5280a35dd86fe4ce3c6
baggzburner <baggzburner@gmail.com>
2024-02-21 13:12:57 +0300

    week 0ne python assignment

---
+++ user_input.py
@@ -0,0 +1,9 @@
+
+
+name = input('what is your name?')
+
+age = input('how old are you?')
+
+location = input('where do you live?')
+
+print ('Good morning '+name+' you are '+age+' years old and you live in '+location)
